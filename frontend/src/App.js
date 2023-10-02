ab2387b742d36ee505649779b6136438f1fe05ab
IF <ivfv@yahoo.com>
2023-10-02 22:54:40 +0200

    Added routing ^

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,13 +2,26 @@ import logo from './logo.svg';
 import './App.css';
 import NavBar from './components/NavBar';
 import Present from './components/Present';
+import Advice from './components/Advice';
+import Footer from './components/Footer';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import FAQ from './pages/FAQ';
 
 
 function App() {
   return (
-    <div>
+    <div className='wrapper'>
+      <BrowserRouter>
+      <Switch>
+      <Route path="/faq">
+        <FAQ />
+        </Route>
       <NavBar />
       <Present />
+      <Advice />
+      <Footer />
+      </Switch>
+      </BrowserRouter>
     </div>
   );
 }
--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -1,5 +1,6 @@
 import React, {useState} from 'react'
 import {AiOutlineClose, AiOutlineMenu} from 'react-icons/ai'
+import FAQ from '../pages/FAQ'
 
 
 const NavBar = () => {
@@ -13,11 +14,11 @@ const NavBar = () => {
     <div className = ' flex justify-between items-center h-18 mx-auto px-4 max-w-[1920px] text-white'> 
         <h1 className='w-full  text-3xl  font-bold pt-4 text-[#cb4f4f]' >CryptoDonate</h1>
         <ul className='hidden md:flex'>
-            <li className='p-4 text-s' >FAQ</li>
-            <li className='p-4 text-s'>Search</li>
-            <li className='p-4 text-s'>Login</li>
-            <li className='p-4 text-s'>Register</li>
-            <li className='p-4 text-s'>Contact</li>
+            <li className='p-4 text-s' ><a href="http://localhost:3000/faq">FAQ</a></li>
+            <li className='p-4 text-s'> <a href="http://localhost:3000/search">Search</a></li>
+            <li className='p-4 text-s'><a href="http://localhost:3000/login">Login</a></li>
+            <li className='p-4 text-s'><a href="http://localhost:3000/register"></a>Register</li>
+            <li className='p-4 text-s'><a href="http://localhost:3000/contacts">Contact</a></li>
         </ul>
         <div onClick={handleNav} className='block md:hidden'>
           {!nav ? <AiOutlineClose size={24}/> : <AiOutlineMenu size={24}/> }
@@ -25,11 +26,11 @@ const NavBar = () => {
         
         <div className={!nav ? 'fixed left-0 h-full top-0 w-[60%]  border-r border-r-gray-500 ease-in-out duration-300 bg-black' : 'fixed left-[-100%]'}>
             <ul className='pt-24 uppercase '>
-            <li className='p-4 border-b border-t border-gray-500 ' >FAQ</li>
-            <li className='p-4 border-b border-t border-gray-500'>Search</li>
-            <li className='p-4 border-b border-t border-gray-500'>Login</li>
-            <li className='p-4 border-b border-t border-gray-500'>Register</li>
-            <li className='p-4 border-b border-t border-gray-500'>Contact</li>
+            <li className='p-4 border-b border-t border-gray-500 ' ><a href="http://localhost:3000/faq">FAQ</a></li>
+            <li className='p-4 border-b border-t border-gray-500'><a href="http://localhost:3000/search">Search</a></li>
+            <li className='p-4 border-b border-t border-gray-500'><a href="http://localhost:3000/login">Login</a></li>
+            <li className='p-4 border-b border-t border-gray-500'><a href="http://localhost:3000/register">Register</a></li>
+            <li className='p-4 border-b border-t border-gray-500'><a href="http://localhost:3000/contacts">Contact</a></li>
             </ul>
         </div>
     </div>
